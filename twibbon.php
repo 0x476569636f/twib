ae26a7c62a753fc0722bff1b2b6c535d0dbbbb54
irvan4iot <52144823+irvan4iot@users.noreply.github.com>
2023-06-23 13:09:51 +0700

    Update twibbon.php

--- twibbon.php
+++ twibbon.php
@@ -41,9 +41,9 @@
 			</a>
 		</div>
 	</nav>
+	<!-- Card untuk memunculkan -->
 	<div class="container my-5">
 		<div class="row">
-			<!-- Card untuk form -->
 			<div class="col-lg-6 offset-lg-3 col-md-12 mb-4">
 				<div class="card card-body shadow-sm mb-4">
 					<?php
@@ -178,9 +178,5 @@
 	});
 
 </script>	
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
